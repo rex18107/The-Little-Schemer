aea2bdba6a64019a617aa8c3c44470c0e5e8be9e
rex18107 <41535795+rex18107@users.noreply.github.com>
2022-08-24 00:05:44 +0800

    did some changes in ch5review

---
+++ extra-excercise.rkt
@@ -0,0 +1,18 @@
+#lang scheme
+
+; 判断是否为复数
+(define (plural? n)
+  (cond
+    ((zero? n) #t)
+    ((= n 1) #f)
+    (else (plural? (sub1 (sub1 n))))))
+(plural? 5)
+; 判断是否为复数
+(define (singular? n)
+  (cond
+    ((plural? n) #f)
+    (else #t)))
+(singular? 3 )
+; 判断是否为质数
+;(define ())
+(< 3 4)
