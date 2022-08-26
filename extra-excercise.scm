353548ba2fae9107c69650812baf99b1dbd2f74d
rex18107 <41535795+rex18107@users.noreply.github.com>
2022-08-26 14:55:21 +0800

    delete a rkt file

--- extra-excercise.scm
+++ extra-excercise.scm
@@ -3,15 +3,19 @@
 ; 判断是否为复数
 (define (plural? n)
   (cond
+    ; 当n等于0时，为结束条件，返回#t
     ((zero? n) #t)
+    ; 当n等于1时,为结束条件,返回#f
     ((= n 1) #f)
+    ; 对n进行减一后递归
     (else (plural? (sub1 (sub1 n))))))
 (plural? 5)
-; 判断是否为复数
+; 判断是否为单数
 (define (singular? n)
   (cond
     ((plural? n) #f)
     (else #t)))
 (singular? 3 )
 ; 判断是否为质数
-(define ())
+;(define ())
+(< 3 4)
