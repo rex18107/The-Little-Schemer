ae3cceb8c2c9ebe97731b2534573c235cb26e92b
rex18107 <41535795+rex18107@users.noreply.github.com>
2022-08-28 00:07:21 +0800

    finish ch6

--- ch6.scm
+++ ch6.scm
@@ -3,10 +3,14 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 ; p97
-'a ; a
-'+ ; +
-'* ; *
-(let ((y 'a)) (eq? 'a y)); #t
+; a
+'a 
+; +
+'+ 
+; *
+'* 
+; #t
+(let ((y 'a)) (eq? 'a y))
 
 ; p100
 ; 判断一个算术表达式的表示方式中，是否除了+，*,^外只包含数字
@@ -22,7 +26,8 @@
      (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp))))))
     ((eq? (car (cdr aexp)) '^)
      (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp))))))))
-(numbered? '(@ 2));  #t
+;  #t
+(numbered? '(@ 2))
 
 ; numbered函数的简化
 (define (numbered-s? aexp)
@@ -31,12 +36,14 @@
     (else
      (and (numbered? (car aexp))
           (numbered? (car (cdr (cdr aexp))))))))
-(numbered-s? '(1 2 2))
+;  #t
+(numbered-s? '(1 * 2))
 ; 求n的m次幂
 (define (^ n m)
   (cond
     ((zero? m) 1)
     (else (* n (* n (- m 1))))))
+; 8
 (^ 2 3)
 ; p103
 ; 求出算术表达式
@@ -44,14 +51,87 @@
   (cond
     ; 先判断表达式是不是原子,是的话返回原子作为算术表达式
     ((atom? nexp) nexp)
-    ; 判断
+    ; 判断列表第二个元素是否为+,是的话返回第一个元素和第三个元素相加的值
     ((eq? (car (cdr nexp)) '+)
      (+ (value (car nexp))
         (value (car (cdr (cdr nexp))))))
+     ; 判断列表第二个元素是否为*,是的话返回第一个元素和第三个元素相乘的值
     ((eq? (car (cdr nexp)) '*)
      (+ (value (car nexp))
         (value (car (cdr (cdr nexp))))))
+     ; 返回第一个元素的第三个元素值的次方
     (else
      (^ (value (car nexp))
         (value (car (cdr (cdr nexp))))))))
+; 10
 (value '(2 + (2 ^ 3)))
+
+; p105
+; 一个算术表达式的表示方式中的第一个子表达式
+(define (1st-sub-exp aexp)
+  (car (cdr aexp)))
+
+; p106
+; 一个算术表达式的表示方式中的第二个子表达式
+(define (2nd-sub-exp aexp)
+  (car (cdr (cdr aexp))))
+
+; 将（car nexp）替换成（operator nexp）
+(define (operator aexp)
+  (car aexp))
+
+; 再定义一次value函数,这也算是规定了算术表达式的表现形式
+(define (value-2 nexp)
+  (cond
+    ((atom? nexp) nexp)
+    ((eq? (operator nexp) +)
+     (+ (value-2 (1st-sub-exp nexp))
+        (value-2 (2nd-sub-exp nexp))))
+     ((eq? (operator nexp) +)
+     (* (value-2 (1st-sub-exp nexp))
+        (value-2 (2nd-sub-exp nexp))))
+     (else
+     (^ (value-2 (1st-sub-exp nexp))
+        (value-2 (2nd-sub-exp nexp))))))
+; 3
+(value-2 (+ 1 2))
+; 30
+(value-2 (* (+ 2 3) (+ 2 4)))
+
+; p108
+; ()代表一个1
+(define (sero? n)
+  (null? n))
+; #t
+(sero? '())
+; #f
+(sero? '(()))
+
+; 加一,用（）表示1
+(define (edd1 n)
+  (cons '() n))
+; (() ())
+(edd1 '(()))
+
+; 减一,用()表示1
+(define (zub1 n)
+  (cdr n))
+; (())
+(zub1 '(() ()))
+
+; 求出n加m的表示法
+(define (++ n m)
+  (cond
+    ((sero? m) n)
+    (else (edd1 (++ n (zub1 m))))))
+; (() () () ())
+(++ '(()) '(() () ()))
+
+; 判断是否全为原子的列表
+(define (lat? l)
+  (cond
+    ((null? l) #t)
+    ((atom? (car l)) (lat? (cdr l)))
+    (else #f)))
+; #f,要小心阴影
+(lat? '(() '(() ()) '(() () ())))
