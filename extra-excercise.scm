6ec101b0c68c3da19bfd7b0f6008f46d5ebeb6dc
rex18107 <41535795+rex18107@users.noreply.github.com>
2022-08-31 15:20:01 +0800

    revise

--- extra-excercise.scm
+++ extra-excercise.scm
@@ -1,30 +1,29 @@
 #lang scheme
 
-; 判断是否为复数
-(define (plural? n)
+; 判断是否为偶数
+(define (even? n)
   (cond
     ; 当n等于0时，为结束条件，返回#t
     ((zero? n) #t)
     ; 当n等于1时,为结束条件,返回#f
     ((= n 1) #f)
     ; 对n进行减一后递归
-    (else (plural? (sub1 (sub1 n))))))
-(plural? 5)
+    (else (even? (sub1 (sub1 n))))))
+(even? 4)
 
 ; 取反
-(define (not-function? n)
+(define (not n)
   (cond
     ; 此引入参数结果如果为#t，返回#f
     ((eq? n #t) #f)
     (else #t)))
-(not-function? (plural? 3))
+(not-function? (even? 3))
+
 ; 判断是否为单数
-(define (singular? n)
-  (cond
-    ; 如果n是偶数返回#f
-    ((plural? n) #f)
-    ; 否则即为#t
-    (else #t))) 
-(singular? 3 )
+(define (odd? n)
+    ; 调用not函数取反
+    (not (even? n))
+(odd? 3 )
+
 ; 判断是否为质数
 ;(define ())
